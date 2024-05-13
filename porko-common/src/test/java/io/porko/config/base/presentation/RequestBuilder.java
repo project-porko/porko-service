33a9b07a9360400ad5e86f8f6b6811bbccd22eee
choi-ys <project.log.062@gmail.com>
2024-05-13 20:17:54 +0900

    config(#2): Presentation 계층 Test 관련 공통 설정 추가
    RequestBuilder: MockMvc를 이용한 요청 생성 간편화를 위한 Utils
    
    WebMvcTestBase: Controller TC 수행 시 필요한 설정 부

---
+++ porko-common/src/test/java/io/porko/config/base/presentation/RequestBuilder.java
@@ -0,0 +1,197 @@
+package io.porko.config.base.presentation;
+
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+public class RequestBuilder {
+    private final MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper;
+
+    private String url;
+
+    private List<Object> urlVariables;
+
+    private HttpMethod method;
+
+    private final Map<String, Object> headers = new HashMap<>();
+    private MediaType contentType;
+
+    private Object content;
+
+    private HttpStatus status;
+
+    private RequestBuilder(final MockMvc mockMvc, final ObjectMapper mapper) {
+        this.mockMvc = mockMvc;
+        this.objectMapper = mapper;
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+    }
+
+    public static EndPoint get(final MockMvc mockMvc, final ObjectMapper objectMapper) {
+        return new RequestBuilder(mockMvc, objectMapper)
+            .request()
+            .get();
+    }
+
+    public static EndPoint post(final MockMvc mockMvc, final ObjectMapper objectMapper) {
+        return new RequestBuilder(mockMvc, objectMapper)
+            .request()
+            .post();
+    }
+
+    public static EndPoint put(final MockMvc mockMvc, final ObjectMapper objectMapper) {
+        return new RequestBuilder(mockMvc, objectMapper)
+            .request()
+            .put();
+    }
+
+    public static EndPoint delete(final MockMvc mockMvc, final ObjectMapper objectMapper) {
+        return new RequestBuilder(mockMvc, objectMapper)
+            .request()
+            .delete();
+    }
+
+    private Method request() {
+        return new Method();
+    }
+
+    public class Method {
+        public EndPoint method(final HttpMethod methodInput) {
+            method = methodInput;
+            return new EndPoint();
+        }
+
+        public EndPoint get() {
+            method = GET;
+            return new EndPoint();
+        }
+
+        public EndPoint post() {
+            method = POST;
+            return new EndPoint();
+        }
+
+        public EndPoint put() {
+            method = PUT;
+            return new EndPoint();
+        }
+
+        public EndPoint delete() {
+            method = DELETE;
+            return new EndPoint();
+        }
+    }
+
+    public class EndPoint {
+        public Content url(final String urlInput, final Object... urlVariablesInput) {
+            url = urlInput;
+            urlVariables = Arrays.asList(urlVariablesInput);
+            return new Content();
+        }
+    }
+
+    public class Content {
+        public Expect jsonContent(final Object object) {
+            contentType = MediaType.APPLICATION_JSON;
+            content = object;
+            return new Expect();
+        }
+
+        public Expect expect() {
+            return new Expect();
+        }
+    }
+
+    public class Expect {
+        public ResultActions expectStatus(HttpStatus expectStatus) throws Exception {
+            status = expectStatus;
+            return makeRequest();
+        }
+
+        public ResultActions ok() throws Exception {
+            status = HttpStatus.OK;
+            return makeRequest();
+        }
+
+        public ResultActions noContent() throws Exception {
+            status = HttpStatus.NO_CONTENT;
+            return makeRequest();
+        }
+
+        public ResultActions created() throws Exception {
+            status = HttpStatus.CREATED;
+            return makeRequest();
+        }
+
+        public ResultActions unAuthorized() throws Exception {
+            status = HttpStatus.UNAUTHORIZED;
+            return makeRequest();
+        }
+
+        public ResultActions forbidden() throws Exception {
+            status = HttpStatus.FORBIDDEN;
+            return makeRequest();
+        }
+
+        public ResultActions badRequest() throws Exception {
+            status = HttpStatus.BAD_REQUEST;
+            return makeRequest();
+        }
+
+        public ResultActions notFound() throws Exception {
+            status = HttpStatus.NOT_FOUND;
+            return makeRequest();
+        }
+
+        public ResultActions conflict() throws Exception {
+            status = HttpStatus.CONFLICT;
+            return makeRequest();
+        }
+    }
+
+    /**
+     * TODO : TC 기반 API docs 추출 시 RequestBuilder 변경
+     * - as-is :MockMvcRequestBuilders
+     * - to-be : RestDocumentationRequestBuilders
+     */
+    private ResultActions makeRequest() throws Exception {
+        MockHttpServletRequestBuilder request =
+            MockMvcRequestBuilders.request(
+                method,
+                url,
+                urlVariables.toArray()
+            );
+
+        for (String header : headers.keySet()) {
+            request.header(header, headers.get(header));
+        }
+
+        if (content != null) {
+            request.contentType(contentType)
+                .content(objectMapper.writeValueAsString(content));
+        }
+
+        return mockMvc.perform(request)
+            .andDo(print())
+            .andExpect(status().is(status.value()));
+    }
+}
---
+++ porko-common/src/test/java/io/porko/config/base/presentation/WebMvcTestBase.java
@@ -0,0 +1,44 @@
+package io.porko.config.base.presentation;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.porko.config.base.TestBase;
+import io.porko.config.security.TestSecurityConfig;
+import jakarta.annotation.Resource;
+import java.io.UnsupportedEncodingException;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@Import(TestSecurityConfig.class)
+public abstract class WebMvcTestBase extends TestBase {
+    @Resource
+    protected MockMvc mockMvc;
+    @Resource
+    protected ObjectMapper objectMapper;
+
+    protected RequestBuilder.EndPoint get() {
+        return RequestBuilder.get(mockMvc, objectMapper);
+    }
+
+    protected RequestBuilder.EndPoint post() {
+        return RequestBuilder.post(mockMvc, objectMapper);
+    }
+
+    protected RequestBuilder.EndPoint put() {
+        return RequestBuilder.put(mockMvc, objectMapper);
+    }
+
+    protected RequestBuilder.EndPoint delete() {
+        return RequestBuilder.delete(mockMvc, objectMapper);
+    }
+
+    protected <T> T andReturn(ResultActions resultActions, Class<T> clazz)
+        throws UnsupportedEncodingException, JsonProcessingException {
+        return objectMapper.readValue(extractResponseBody(resultActions), clazz);
+    }
+
+    private String extractResponseBody(ResultActions resultActions) throws UnsupportedEncodingException {
+        return resultActions.andReturn().getResponse().getContentAsString();
+    }
+}
