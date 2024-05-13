cb59a7cb1a25658a016cf698aa193aa3a3e30860
choi-ys <project.log.062@gmail.com>
2024-05-13 22:30:26 +0900

    feat(#1): 회원 도메인 객체 Entity 변환
    회원 아이디, 회원 이메일 항목에 Unique 제약 조건 설정
    @Column을 이용한 각 컬럼의 Nullable 여부 및 길이 설정
    Auditing을 이용한 생성/수정일시 메타 데이터 적용을 위한 TimeMetaFields 상속

--- porko-service/src/main/java/io/porko/member/domain/Member.java
+++ porko-service/src/main/java/io/porko/member/domain/Member.java
@@ -1,13 +1,53 @@
 package io.porko.member.domain;
 
-public class Member {
+import io.porko.config.jpa.auditor.TimeMetaFields;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(
+    uniqueConstraints = {
+        @UniqueConstraint(name = "UK_MEMBER_ID", columnNames = "member_id"),
+        @UniqueConstraint(name = "UK_MEMBER_EMAIL", columnNames = "email")
+    })
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends TimeMetaFields {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 20)
     private String memberId;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false, length = 10)
     private String name;
+
+    @Column(nullable = false, length = 40)
     private String email;
+
+    @Column(nullable = false, length = 11)
     private String phoneNumber;
+
+    @Embedded
     private Address address;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
     public Member(
