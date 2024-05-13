dea8936f60c51720883f29431e328f16de10f6e9
choi-ys <project.log.062@gmail.com>
2024-05-13 20:21:47 +0900

    config(#2): Test 객체 생성 간편화를 위한 Fixture Monkey 설정
    Fixture monkey 사용을 위한 'fixture-monkey-starter:1.0.14' 의존성 추가
    
    FixtureCommon: Dto, Entity, Builder 등 여러 타입의 객체 생성을 위한 FixtureMonkey 설정 부

--- porko-common/build.gradle.kts
+++ porko-common/build.gradle.kts
@@ -11,4 +11,6 @@ dependencies {
     implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter:1.0.14")
 }
