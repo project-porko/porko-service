8f09c42bd72119cea2ef3a44fd6dc9f8e11d8ae8
choi-ys <project.log.062@gmail.com>
2024-05-13 18:11:30 +0900

    init(#2): 공통 설정을 위한 모듈 생성
    settings.gradle.kts에 공통 설정을 위해 생성한 모듈 등록

---
+++ porko-common/src/main/java/io/porko/PorkoCommonConfig.java
@@ -0,0 +1,9 @@
+package io.porko;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public abstract class PorkoCommonConfig {
+}
