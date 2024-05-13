e6e775c008ff38d188b40d85b6f2809a1f0fb3ed
choi-ys <project.log.062@gmail.com>
2024-05-13 19:10:16 +0900

    config(#2): Spring Security 관련 공통 설정 추가
    Application 인증/인가를 위한 'spring-boot-starter-security' 의존성 추가
    
    비밀번호 암호화를 위해 Spring Security에서 제공하는 PasswordEncoder Bean 등록 설정 추가
    
    JWT를 이용한 인증/인가를 위한 기본 SecurityFilterChain 설정

--- porko-common/build.gradle.kts
+++ porko-common/build.gradle.kts
@@ -9,4 +9,5 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 }
---
+++ porko-common/src/main/java/io/porko/config/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package io.porko.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .headers(headersConfigurer -> headersConfigurer
+                .frameOptions(FrameOptionsConfig::disable)
+            )
+            .sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            )
+            .build();
+    }
+}
