2b85f052f7fdbbbd58d8a361d0d178b5fc55e1f1
choi-ys <project.log.062@gmail.com>
2024-05-13 17:45:18 +0900

    init: initial project
    - spring boot: 3.2.5
    - dependency-management: 1.1.4
    - java 17
    
    project for porko
    [project porko](https://github.com/project-porko)

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+val bootJar: org.springframework.boot.gradle.tasks.bundling.BootJar by tasks
+
+bootJar.enabled = false
+
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "io.porko"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.springframework.boot")
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        annotationProcessor("org.projectlombok:lombok")
+        compileOnly("org.projectlombok:lombok")
+
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
