a0d7ad3cdac2813b5c547b02e3d8ac6e3cbd0923
choi-ys <project.log.062@gmail.com>
2024-05-13 19:27:01 +0900

    chore(#2): 일부 설정에 미 구현된 내용에 대한 TODO 추가
    TestJpaConfiguration: Test 환경에서 AuditingListener가 메타 데이터 설정을 위해 필요한 정보 제공
    TestSecurityConfig: Mocking을 통해 인증된 사용자 정보 제공

--- porko-common/src/test/java/io/porko/config/jpa/TestJpaConfiguration.java
+++ porko-common/src/test/java/io/porko/config/jpa/TestJpaConfiguration.java
@@ -3,6 +3,7 @@ package io.porko.config.jpa;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// TODO: Test 환경에서 사용할 사용자 정보를 통해 AuditingListener가 설정할 메타 데이터 제공
 @TestConfiguration
 @EnableJpaAuditing(modifyOnCreate = false)
 public class TestJpaConfiguration {
--- porko-common/src/test/java/io/porko/config/security/TestSecurityConfig.java
+++ porko-common/src/test/java/io/porko/config/security/TestSecurityConfig.java
@@ -2,6 +2,7 @@ package io.porko.config.security;
 
 import org.springframework.context.annotation.Import;
 
+// TODO: Mocking을 통해 인증된 사용자 정보 제공
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
 }
