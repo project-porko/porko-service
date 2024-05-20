cac5c7fc38e5a78f4b55fedf6b6a623deb846828
choi-ys <project.log.062@gmail.com>
2024-05-21 01:34:11 +0900

    feat(#9): 인증 필터 구현
    인증 필터
    - 인증이 필요한 요청 Header에서 JWT를 추출 및 검증
    - JWT로부터 Application에서 사용하기 위해 필요한 인증 정보인 Principal 추출
    - 인증 정보 저장소인 SecurityContextHolder에 추출한 Principal 저장
    
    인증 정책 추가
    - 인증 필터 등록
    - 인증이 필요하지 않은 요청(Http method + Endpoint) 정책 등록

--- porko-service/src/main/java/io/porko/auth/config/SecurityConfig.java
+++ porko-service/src/main/java/io/porko/auth/config/SecurityConfig.java
@@ -1,17 +1,25 @@
 package io.porko.auth.config;
 
+import io.porko.auth.config.jwt.JwtProperties;
+import io.porko.auth.filter.TokenVerifyFilter;
+import io.porko.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthService authService;
+    private final JwtProperties jwtProperties;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -24,8 +32,15 @@ public class SecurityConfig {
             .sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
+            // [TODO:Refactor] endpoint, roles에 따른 인가 정책 가독성 개선을 위한 AuthenticationEndpointByRoles 적용
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
+                .requestMatchers(HttpMethod.POST, "/member/sign-up", "/login").permitAll()
+                .requestMatchers(HttpMethod.GET, "/member/validate").permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(
+                new TokenVerifyFilter(authService, jwtProperties),
+                UsernamePasswordAuthenticationFilter.class
             )
             .build();
     }
