4014b3737b97b76ba4cc0911e44c6def238e8e2e
choi-ys <project.log.062@gmail.com>
2024-05-21 05:15:15 +0900

    feat(#18): querydsl을 이용한 회원 조회 쿼리 추가
    querydsl test를 위한 설정 추가
    test를 위한 dummy data 생성 스크립트 작성

---
+++ porko-service/src/main/java/io/porko/member/repo/MemberQueryRepo.java
@@ -0,0 +1,30 @@
+package io.porko.member.repo;
+
+import static io.porko.member.domain.QMember.member;
+
+import com.querydsl.jpa.JPQLQueryFactory;
+import io.porko.member.controller.model.MemberResponse;
+import io.porko.member.controller.model.QMemberResponse;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberQueryRepo {
+    private final JPQLQueryFactory queryFactory;
+
+    public Optional<MemberResponse> findMemberById(Long id) {
+        return Optional.ofNullable(queryFactory.select(
+                new QMemberResponse(
+                    member.id,
+                    member.memberId,
+                    member.name,
+                    member.email,
+                    member.createdAt
+                ))
+            .from(member)
+            .where(member.id.eq(id))
+            .fetchOne());
+    }
+}
