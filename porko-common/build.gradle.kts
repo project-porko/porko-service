64628331c5608d87c346eb281b7cdb3ec6c3f160
choi-ys <project.log.062@gmail.com>
2024-05-21 05:16:38 +0900

    chore(#18): porko-common에서 더이상 사용하지 않는 spring security 의존성 제거

--- porko-common/build.gradle.kts
+++ porko-common/build.gradle.kts
@@ -9,7 +9,6 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter:1.0.14")
