f3972d99eb0997475dcc255cf1677ebac8a1621f
choi-ys <project.log.062@gmail.com>
2024-05-21 00:55:22 +0900

    chore(#9): 코드 리팩토링으로 인해 더이상 사용하지 않는 메서드 제거

--- porko-common/src/main/java/io/porko/utils/ConvertUtils.java
+++ porko-common/src/main/java/io/porko/utils/ConvertUtils.java
@@ -1,11 +1,5 @@
 package io.porko.utils;
 
-import java.lang.reflect.Field;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-
 public class ConvertUtils {
     public static String convertToConstants(String value) {
         String regex = "([a-z])([A-Z])";
@@ -13,24 +7,5 @@ public class ConvertUtils {
 
         return value.replaceAll(regex, replacement)
             .toUpperCase();
-    }
-
-    public static Map<String, Object> convertToMap(Object obj) {
-        try {
-            if (Objects.isNull(obj)) {
-                return Collections.emptyMap();
-            }
-            Map<String, Object> convertMap = new HashMap<>();
-
-            Field[] fields = obj.getClass().getDeclaredFields();
-
-            for (Field field : fields) {
-                field.setAccessible(true);
-                convertMap.put(field.getName(), field.get(obj));
-            }
-            return convertMap;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
