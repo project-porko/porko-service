55f993f6d1337a9514dedb6ee4d20e71af0c0a0b
choi-ys <project.log.062@gmail.com>
2024-05-21 01:02:47 +0900

    build(#9): JWT를 이용한 인증 처리를 위한 jjwt 의존성 추가

--- porko-service/build.gradle.kts
+++ porko-service/build.gradle.kts
@@ -1,11 +1,19 @@
 val testSourceSet: SourceSetOutput = project(":porko-common").sourceSets["test"].output
 
+object DependencyVersions {
+    const val JJWT_VERSION = "0.11.5"
+}
+
 dependencies {
     implementation(project(":porko-common"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("io.jsonwebtoken:jjwt-api:${DependencyVersions.JJWT_VERSION}")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:${DependencyVersions.JJWT_VERSION}")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:${DependencyVersions.JJWT_VERSION}")
 
     testRuntimeOnly("com.h2database:h2")
     testImplementation(testSourceSet)
