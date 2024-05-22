3dcc11fbb71b8fbf8f753caba350fcdd740b76a8
Jian,Kim <loveinansan1@gmail.com>
2024-05-22 18:14:48 +0900

    feat(#29): 입출금 내역 도메인 설계
    입출금 내역 도메인 항목
    -cost: 입출금 금액
    -regret: 후회 여부
    -place: 결제처
    -payType: 입출금 타입
    -usedAt: 결제 일시
    -spendingCategoryId: 소비 카테고리
      - name: 카테고리 이름
      - url: 이미지 URL
    -type: 수입, 지출
    -memo: 메모
    
    객체 생성을 위한 생성자 및 정적 팩토리 메서드 추가

---
+++ porko-service/src/main/java/io/porko/history/domain/History.java
@@ -0,0 +1,75 @@
+package io.porko.history.domain;
+
+import io.porko.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class History {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal cost;
+
+    @Column(nullable = false)
+    private Boolean regret;
+
+    @Column(nullable = false, length = 30)
+    private String place;
+
+    @Column(nullable = false, length = 30)
+    private String payType;
+
+    @Column(nullable = false)
+    private LocalDateTime usedAt;
+
+    @Embedded
+    private SpendingCategory spendingCategoryId;
+
+    @Column(nullable = false, length = 10)
+    private String type;
+
+    @Column(nullable = false, length = 100)
+    private String memo;
+
+    public History(
+            BigDecimal cost,
+            Boolean regret,
+            String place,
+            String payType,
+            LocalDateTime usedAt,
+            SpendingCategory spendingCategoryId,
+            String type,
+            String memo
+    ) {
+        this.cost = cost;
+        this.regret = regret;
+        this.place = place;
+        this.payType = payType;
+        this.usedAt = usedAt;
+        this.spendingCategoryId = spendingCategoryId;
+        this.type = type;
+        this.memo = memo;
+    }
+
+    public static History of(
+            BigDecimal cost,
+            Boolean regret,
+            String place,
+            String payType,
+            LocalDateTime usedAt,
+            SpendingCategory spendingCategoryId,
+            String type,
+            String memo
+    ) {
+        return new History(cost, regret, place, payType, usedAt, spendingCategoryId, type, memo);
+    }
+}
