81b37235b874e67e6b872d135737a57ed887f90f
ndy <ratcomp9992@gmail.com>
2024-05-22 18:09:35 +0900

    feat(#30): 목표 예산 도메인 설계
    목표 예산 도메인 항목
    - id: 목표 예산 ID
    - memberId: 회원 ID
    - goalCost: 목표 예산
    - yearMonth: 목표 년/월
    
    객체 생성을 위한 생성자 및 정적 팩토리 메서드 추가

---
+++ porko-service/src/main/java/io/porko/budget/domain/Budget.java
@@ -0,0 +1,44 @@
+package io.porko.budget.domain;
+
+import io.porko.utils.ConvertUtils;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.YearMonth;
+
+@Entity
+@NoArgsConstructor
+public class Budget {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private BigDecimal goalCost;
+
+    @Column(nullable = false)
+    private YearMonth yearMonth;
+
+    private Budget(
+            Long memberId,
+            BigDecimal goalCost,
+            String yearMonth
+    ) {
+        this.memberId = memberId;
+        this.goalCost = goalCost;
+        this.yearMonth = ConvertUtils.StringToYearMonth(yearMonth);
+    }
+
+    public static Budget of(
+            Long memberId,
+            BigDecimal goalCost,
+            String yearMonth
+    ) {
+        return new Budget(memberId, goalCost, yearMonth);
+    }
+}
