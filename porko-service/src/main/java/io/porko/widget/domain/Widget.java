6f92c4cc2356f1417264cd1d4620fd2974ecca44
choi-ys <project.log.062@gmail.com>
2024-05-24 21:54:45 +0900

    feat(#39): 위젯 Entity 설계
    위젯 중복 방지를 위해 설정한 WidgetCode 컬럼의 Unique 제약 조건 명시
    
    위젯 도메인 관련 객체 설계
    - WidgetCode : 코드값, 코드 설명, 타입으로 구성된 Enum
    - WidgetType : 고정형, 기본형, 선택형으로 구분되는 위젯의 성격을 표현

---
+++ porko-service/src/main/java/io/porko/widget/domain/Widget.java
@@ -0,0 +1,36 @@
+package io.porko.widget.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(
+    name = "UK_WIDGET_CODE", columnNames = "widgetCode")
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Widget {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private WidgetCode widgetCode;
+
+    private Widget(WidgetCode widgetCode) {
+        this.widgetCode = widgetCode;
+    }
+
+    public static Widget from(WidgetCode widgetCode) {
+        return new Widget(widgetCode);
+    }
+}
