da257c5677f52c5ee389279ceb07d9bc175c4c6f
choi-ys <project.log.062@gmail.com>
2024-05-27 07:12:47 +0900

    refactor(#43): CacheType 필드 기본값 설정
    Cache 이름은 상수에 명시적으로 표기, 기타 항목은 기본값을 가지도록 설정
    - TTL : 24시간
    - maxElementSize : 10000개

--- porko-service/src/main/java/io/porko/config/cache/CacheType.java
+++ porko-service/src/main/java/io/porko/config/cache/CacheType.java
@@ -1,14 +1,26 @@
 package io.porko.config.cache;
 
+import static io.porko.config.cache.CacheType.DefaultCacheProperties.DEFAULT_MAX_ELEMENTS_SIZE;
+import static io.porko.config.cache.CacheType.DefaultCacheProperties.TIME_TO_LIVE_HOURS;
+
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
-enum CacheType {
-    WIDGETS("widgets", 24, 10000);
+public enum CacheType {
+    WIDGETS("widgets");
 
     private final String name;
     private final long timeToLiveHours;
     private final int maxElementSize;
+
+    CacheType(String name) {
+        this.name = name;
+        this.timeToLiveHours = TIME_TO_LIVE_HOURS;
+        this.maxElementSize = DEFAULT_MAX_ELEMENTS_SIZE;
+    }
+
+    static class DefaultCacheProperties {
+        static final int TIME_TO_LIVE_HOURS = 24;
+        static final int DEFAULT_MAX_ELEMENTS_SIZE = 10000;
+    }
 }
