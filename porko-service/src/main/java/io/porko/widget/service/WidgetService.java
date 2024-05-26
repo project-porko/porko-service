19116180a979806a69dad066c1e624fff8f9ff20
choi-ys <project.log.062@gmail.com>
2024-05-27 07:20:51 +0900

    refactor(#43): 모든 위젯 목록 조회 로직 시 Cache 적용
    @Cacheable을 이용한 반환값 캐싱
    - cacheNames : CacheType의 cacheName
    - key : SPEL을 이용한 현재 수행 메서드 명
    
    TC 작성
    - 반복 적인 서비스 호출 시, 첫번째 호출 이후 값 반환을 위한 Repo 호출이 아닌 Cache를 통한 값 반환 여부 검증

--- porko-service/src/main/java/io/porko/widget/service/WidgetService.java
+++ porko-service/src/main/java/io/porko/widget/service/WidgetService.java
@@ -4,6 +4,7 @@ package io.porko.widget.service;
 import io.porko.widget.controller.model.WidgetsResponse;
 import io.porko.widget.repo.WidgetRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class WidgetService {
     private final WidgetRepo widgetRepo;
 
+    @Cacheable(cacheNames = "widgets", key = "#root.methodName")
     public WidgetsResponse loadAllWidgets() {
         return WidgetsResponse.from(widgetRepo.findAll());
     }
