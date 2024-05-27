38ec8d6d1ae4ea3724546a57eb1b9da4b50024b3
ndy <ratcomp9992@gmail.com>
2024-05-27 16:32:28 +0900

    chore(#30): goalDate String으로 변경

--- porko-service/src/main/java/io/porko/budget/repo/BudgetRepo.java
+++ porko-service/src/main/java/io/porko/budget/repo/BudgetRepo.java
@@ -3,9 +3,8 @@ package io.porko.budget.repo;
 import io.porko.budget.domain.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.YearMonth;
 import java.util.Optional;
 
 public interface BudgetRepo extends JpaRepository<Budget, Long> {
-    Optional<Budget> findByGoalDateAndMemberId(YearMonth goalDate, Long memberId);
+    Optional<Budget> findByGoalDateAndMemberId(String goalDate, Long memberId);
 }
--- porko-service/src/main/java/io/porko/budget/service/BudgetService.java
+++ porko-service/src/main/java/io/porko/budget/service/BudgetService.java
@@ -6,11 +6,13 @@ import io.porko.budget.exception.BudgetErrorCode;
 import io.porko.budget.exception.BudgetException;
 import io.porko.budget.repo.BudgetRepo;
 import io.porko.history.repo.HistoryQueryRepo;
+import io.porko.utils.ConvertUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.YearMonth;
 
 @Service
@@ -21,9 +23,12 @@ public class BudgetService {
     private final HistoryQueryRepo historyQueryRepo;
 
     public BudgetResponse getBudget(YearMonth goalDate, Long memberId) {
-        Budget budget = budgetRepo.findByGoalDateAndMemberId(goalDate, memberId).orElseThrow(() -> new BudgetException(BudgetErrorCode.NOT_FOUND, goalDate, memberId));
+        Budget budget = budgetRepo.findByGoalDateAndMemberId(ConvertUtils.YearMonthToString(goalDate), memberId).orElseThrow(() -> new BudgetException(BudgetErrorCode.NOT_FOUND, goalDate, memberId));
+
         BigDecimal totalCost = historyQueryRepo.calcTotalCost(goalDate, memberId).orElse(BigDecimal.ZERO);
 
-        return BudgetResponse.of(totalCost.divide(budget.getGoalCost()).multiply(BigDecimal.valueOf(100)).abs());
+        return BudgetResponse.of(totalCost.divide(budget.getGoalCost())
+                            .multiply(BigDecimal.valueOf(100))
+                            .abs());
     }
 }
