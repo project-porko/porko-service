9ccdd5841fa45420d09b9ab0caed116bfb949ef5
ndy <ratcomp9992@gmail.com>
2024-05-27 18:31:55 +0900

    style(#30): 목표 예산 미설정 에러 코드 변경

--- porko-service/src/main/java/io/porko/budget/exception/BudgetErrorCode.java
+++ porko-service/src/main/java/io/porko/budget/exception/BudgetErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum BudgetErrorCode {
-    NOT_FOUND(HttpStatus.NOT_FOUND, "사용자가 목표 금액을 설정하지 않았습니다. [request: %s]");
+    BUDGET_NOT_SET(HttpStatus.NOT_FOUND, "사용자가 목표 금액을 설정하지 않았습니다. [request: %s]");
 
     private final HttpStatus status;
     private final String message;
