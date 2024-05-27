4a480315e16bcf3328cadd5ddcb06bfe73813158
ndy <ratcomp9992@gmail.com>
2024-05-27 12:18:49 +0900

    chore(#30): 목표 예산 사용률 API QueryDSL 메서드 삭제

--- porko-service/src/main/java/io/porko/budget/repo/BudgetQueryRepo.java
+++ porko-service/src/main/java/io/porko/budget/repo/BudgetQueryRepo.java
@@ -1,27 +1,11 @@
 package io.porko.budget.repo;
 
 import com.querydsl.jpa.JPQLQueryFactory;
-import io.porko.budget.controller.model.BudgetResponse;
-import io.porko.budget.controller.model.QBudgetResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-import static io.porko.budget.domain.QBudget.budget;
 
 @Repository
 @RequiredArgsConstructor
 public class BudgetQueryRepo {
     private final JPQLQueryFactory queryFactory;
-
-    public Optional<BudgetResponse> findThisMonthBudgetById(Long id) {
-        return Optional.ofNullable(queryFactory.select(
-                new QBudgetResponse(
-                    budget.goalCost
-                ))
-            .from(budget)
-            .where(budget.memberId.eq(id))
-            .fetchOne());
-    }
 }
