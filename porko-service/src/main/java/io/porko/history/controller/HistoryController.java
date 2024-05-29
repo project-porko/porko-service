a4ac22cb7950154cf23ecee2a6154d89770cb843
Jian,Kim <loveinansan1@gmail.com>
2024-05-29 14:52:39 +0900

    #feat(#29): 입출금 내역 컨트롤러
    - 로그인한 사용자의 히스토리 조회 API 추가
    - startDate와 endDate를 사용하여 특정 기간의 히스토리를 조회
    - 날짜 범위가 지정되지 않은 경우 전체 히스토리 조회
    - HistoryService의 getHistoryList 메서드를 통해 조건에 따라 히스토리 반환
    - @LoginMember 어노테이션을 사용하여 로그인된 사용자 ID 주입

---
+++ porko-service/src/main/java/io/porko/history/controller/HistoryController.java
@@ -0,0 +1,32 @@
+package io.porko.history.controller;
+
+
+import io.porko.auth.controller.model.LoginMember;
+import io.porko.history.controller.model.HistoryResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import io.porko.history.service.HistoryService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+public class HistoryController {
+    private final HistoryService historyService;
+
+    @GetMapping("history")
+    ResponseEntity<List<HistoryResponse>> getHistoryList(
+            @LoginMember Long loginMemberId,
+            @RequestParam(value = "startDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(value = "endDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+
+        List<HistoryResponse> historyList = historyService.getHistoryList(loginMemberId, startDate, endDate);
+        return ResponseEntity.ok(historyList);
+    }
+}
