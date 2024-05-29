3e63aff401f06d171eab47f73876d86399de7f18
choi-ys <project.log.062@gmail.com>
2024-05-29 14:14:29 +0900

    test(#40): fixture-monkey 관련 Util 추가
    - Unique한 Fixture 생성을 위한 ArbitraryGenerator 추가
    - Long, int 타입에 순차적인 순서 부여를 위한 Variable 생성 부 추가
      - 전체 TC에서 접근 할 수 있도록 TestBase에 구현

--- porko-common/src/test/java/io/porko/config/fixture/FixtureCommon.java
+++ porko-common/src/test/java/io/porko/config/fixture/FixtureCommon.java
@@ -1,33 +1,33 @@
 package io.porko.config.fixture;
 
 import com.navercorp.fixturemonkey.FixtureMonkey;
-import com.navercorp.fixturemonkey.FixtureMonkeyBuilder;
 import com.navercorp.fixturemonkey.api.introspector.BuilderArbitraryIntrospector;
 import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
 import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
+import com.navercorp.fixturemonkey.customizer.InnerSpec;
 import com.navercorp.fixturemonkey.jakarta.validation.plugin.JakartaValidationPlugin;
 import net.jqwik.api.Arbitraries;
 import net.jqwik.api.arbitraries.StringArbitrary;
 
 public class FixtureCommon {
-    private static final FixtureMonkeyBuilder builder = FixtureMonkey.builder();
-
     public static FixtureMonkey entityType() {
-        return builder
+        return FixtureMonkey.builder()
             .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE)
             .defaultNotNull(true)
             .build();
     }
 
     public static FixtureMonkey dtoType() {
-        return builder
+        return FixtureMonkey.builder()
             .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
             .defaultNotNull(true)
+            .plugin(new JakartaValidationPlugin())
             .build();
     }
 
     public static FixtureMonkey withValidated() {
-        return builder
+        return FixtureMonkey.builder()
+            .defaultArbitraryGenerator(UniqueArbitraryGenerator::new)
             .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
             .defaultNotNull(true)
             .plugin(new JakartaValidationPlugin())
@@ -35,9 +35,13 @@ public class FixtureCommon {
     }
 
     public static FixtureMonkey builderType() {
-        return builder
+        return FixtureMonkey.builder()
             .objectIntrospector(BuilderArbitraryIntrospector.INSTANCE)
             .build();
+    }
+
+    public static InnerSpec setProperty(String property, Object value) {
+        return new InnerSpec().property(property, value);
     }
 
     public static StringArbitrary randomString() {
