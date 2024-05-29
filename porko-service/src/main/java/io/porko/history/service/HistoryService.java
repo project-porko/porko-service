ee9c8a5621a2a5b549d5d88250f161dc689f3c39
Jian,Kim <loveinansan1@gmail.com>
2024-05-29 17:43:49 +0900

    refactor(#29): HistoryService 전체 입출금내역 조회-> 이번달 입출금내역 조회로 변경으로 인한 코드 정리 및 중복코드 제거 및 수정.

--- porko-service/src/main/java/io/porko/history/service/HistoryService.java
+++ porko-service/src/main/java/io/porko/history/service/HistoryService.java
@@ -10,9 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.util.List;
 import java.util.stream.Collectors;
-
 
 @Service
 @Transactional(readOnly = true)
@@ -21,37 +21,24 @@ public class HistoryService {
     private final HistoryRepo historyRepo;
     private final MemberQueryRepo memberQueryRepo;
 
-    public List<HistoryResponse> getHistoryList(Long loginMemberId, LocalDate startDate, LocalDate endDate) {
-        if (startDate != null && endDate != null) {
-            return getHistoryListByDate(loginMemberId, startDate, endDate);
-        } else {
-            return getAllHistoryList(loginMemberId);
-        }
+    public List<HistoryResponse> getThisMonthHistoryList(Long loginMemberId) {
+        LocalDate now = LocalDate.now();
+        YearMonth thisMonth = YearMonth.from(now);
+        LocalDate startDate = thisMonth.atDay(1);
+        LocalDate endDate = thisMonth.atEndOfMonth();
+        return fetchHistoryList(loginMemberId, startDate, endDate);
     }
 
-    private List<HistoryResponse> getHistoryListByDate(Long loginMemberId, LocalDate startDate, LocalDate endDate) {
-        loadMemberById(loginMemberId); // 회원 검증을 위한 호출
+    public List<HistoryResponse> getHistoryListByDate(Long loginMemberId, LocalDate startDate, LocalDate endDate) {
+        return fetchHistoryList(loginMemberId, startDate, endDate);
+    }
+
+    private List<HistoryResponse> fetchHistoryList(Long loginMemberId, LocalDate startDate, LocalDate endDate) {
+        loadMemberById(loginMemberId);
         LocalDateTime startDateTime = startDate.atStartOfDay();
         LocalDateTime endDateTime = endDate.atTime(23, 59, 59);
 
         List<History> histories = historyRepo.findByMemberIdAndUsedAtBetween(loginMemberId, startDateTime, endDateTime);
-
-        return histories.stream()
-                .map(history -> new HistoryResponse(
-                        history.getType(),
-                        history.getUsedAt(),
-                        history.getCost(),
-                        history.getPlace(),
-                        history.getSpendingCategoryId(),
-                        history.getPayType()
-                ))
-                .collect(Collectors.toList());
-    }
-
-    private List<HistoryResponse> getAllHistoryList(Long loginMemberId) {
-        loadMemberById(loginMemberId); // 회원 검증을 위한 호출
-
-        List<History> histories = historyRepo.findByMemberId(loginMemberId);
 
         return histories.stream()
                 .map(history -> new HistoryResponse(
