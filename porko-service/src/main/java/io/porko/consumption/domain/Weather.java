acd01d7cd9e46aa9ee29b52b771d2a5cad29bff8
ndy <ratcomp9992@gmail.com>
2024-06-03 02:26:00 +0900

    feat(#52): Image URL 필드를 get 메서드로 변경

--- porko-service/src/main/java/io/porko/consumption/domain/Weather.java
+++ porko-service/src/main/java/io/porko/consumption/domain/Weather.java
@@ -1,19 +1,19 @@
 package io.porko.consumption.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 public enum Weather {
-    RAINBOW("무지출이 뜬 날", "rainbowURL"),
-    SUN("지출 하늘이 맑은 날", "sunURL"),
-    CLOUD("지출 구름이 낀 날", "cloudURL"),
-    RAIN("지출 비가 내리는 날", "rainURL"),
-    THUNDER("지출 번개가 치는 날", "thunderURL");
+    RAINBOW("무지출이 뜬 날"),
+    SUN("지출 하늘이 맑은 날"),
+    CLOUD("지출 구름이 낀 날"),
+    RAIN("지출 비가 내리는 날"),
+    THUNDER("지출 번개가 치는 날");
 
-    public final String name;
-    public final String imageUrl;
+    public final String weatherName;
 
     public static Weather getWeatherByDailyUsed(BigDecimal dailyUsed) {
         if (dailyUsed.compareTo(BigDecimal.ZERO) == 0) {
@@ -27,5 +27,9 @@ public enum Weather {
         } else {
             return THUNDER;
         }
+    }
+
+    public static String getImageURL() {
+        return "ImageURL";
     }
 }
