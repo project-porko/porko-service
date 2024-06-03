d7fecdb75af28975befa1e6bea1e90180d1d1bdc
ndy <ratcomp9992@gmail.com>
2024-06-03 16:51:52 +0900

    refactor(#52): 소비 날씨 코드 리팩토링

--- porko-service/src/main/java/io/porko/consumption/domain/Weather.java
+++ porko-service/src/main/java/io/porko/consumption/domain/Weather.java
@@ -1,34 +1,74 @@
 package io.porko.consumption.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 @AllArgsConstructor
+@RequiredArgsConstructor
 public enum Weather {
-    RAINBOW("무지출이 뜬 날"),
-    SUN("지출 하늘이 맑은 날"),
-    CLOUD("지출 구름이 낀 날"),
-    RAIN("지출 비가 내리는 날"),
-    THUNDER("지출 번개가 치는 날");
+    RAINBOW("무지출이 뜬 날", 0) {
+        @Override
+        boolean isMatched(BigDecimal cost) {
+            return isRange(startRange, cost);
+        }
+    },
+    SUN("지출 하늘이 맑은 날", 0, 33) {
+        @Override
+        boolean isMatched(BigDecimal cost) {
+            return isRange(startRange, endRange, cost);
+        }
+    },
+    CLOUD("지출 구름이 낀 날", 33, 66) {
+        @Override
+        boolean isMatched(BigDecimal cost) {
+            return isRange(startRange, endRange, cost);
+        }
+    },
+    RAIN("지출 비가 내리는 날", 66, 100) {
+        @Override
+        boolean isMatched(BigDecimal cost) {
+            return isRange(startRange, endRange, cost);
+        }
+    },
+    THUNDER("지출 번개가 치는 날", 100) {
+        @Override
+        boolean isMatched(BigDecimal cost) {
+            return isRange(startRange, cost);
+        }
+    };
 
     public final String weatherName;
+    final int startRange;
+    int endRange;
 
-    public static Weather getWeatherByDailyUsed(BigDecimal dailyUsed) {
-        if (dailyUsed.compareTo(BigDecimal.ZERO) == 0) {
-            return RAINBOW;
-        } else if (dailyUsed.compareTo(BigDecimal.valueOf(0)) > 0 && dailyUsed.compareTo(BigDecimal.valueOf(33)) < 0) {
-            return SUN;
-        } else if (dailyUsed.compareTo(BigDecimal.valueOf(33)) >= 0 && dailyUsed.compareTo(BigDecimal.valueOf(66)) < 0) {
-            return CLOUD;
-        } else if (dailyUsed.compareTo(BigDecimal.valueOf(66)) >= 0 && dailyUsed.compareTo(BigDecimal.valueOf(100)) < 0) {
-            return RAIN;
-        } else {
-            return THUNDER;
+    abstract boolean isMatched(BigDecimal cost);
+
+    public boolean isRange(int start, int end, BigDecimal cost) {
+        BigDecimal startRange = BigDecimal.valueOf(start);
+        BigDecimal endRange = BigDecimal.valueOf(end);
+
+        if (start == 0) {
+            return cost.compareTo(startRange) > 0 && cost.compareTo(endRange) < 0;
         }
+
+        return cost.compareTo(startRange) >= 0 && cost.compareTo(endRange) < 0;
+    }
+
+    public boolean isRange(int start, BigDecimal cost) {
+        return cost.compareTo(BigDecimal.valueOf(start)) == 0;
     }
 
     public static String getImageURL() {
         return "ImageURL";
+    }
+
+    public static Weather getWeatherByDailyUsed(BigDecimal dailyUsed) {
+        return Arrays.stream(values())
+                .filter(it -> it.isMatched(dailyUsed))
+                .findFirst()
+                .orElse(THUNDER);
     }
 }
