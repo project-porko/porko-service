092a60e4683b0c4479c256f61e48c0a01594deb8
choi-ys <project.log.062@gmail.com>
2024-06-05 09:55:03 +0900

    config: https 처리를 위한 swagger 설정 추가

--- porko-service/src/main/java/io/porko/config/swagger/SwaggerConfig.java
+++ porko-service/src/main/java/io/porko/config/swagger/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,6 +26,7 @@ public class SwaggerConfig {
         return new OpenAPI()
             .components(new Components())
             .addSecurityItem(securityRequirement)
+            .addServersItem(new Server().url("/"))
             .components(components);
     }
 }
