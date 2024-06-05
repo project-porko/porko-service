8887c2f4cc9f51dab3f788180fbb40c55ed62de9
ndy <ratcomp9992@gmail.com>
2024-06-05 16:57:05 +0900

    feat(#101): String을 LocalDate로 변환하는 Util메서드 구현

--- porko-common/src/main/java/io/porko/utils/ConvertUtils.java
+++ porko-common/src/main/java/io/porko/utils/ConvertUtils.java
@@ -1,5 +1,8 @@
 package io.porko.utils;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class ConvertUtils {
     public static String convertToConstants(String value) {
         String regex = "([a-z])([A-Z])";
@@ -7,5 +10,11 @@ public class ConvertUtils {
 
         return value.replaceAll(regex, replacement)
             .toUpperCase();
+    }
+
+    public static LocalDate convertStringToLocalDate(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        return LocalDate.parse(date, formatter);
     }
 }
