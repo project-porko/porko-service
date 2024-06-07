06eb77e112385f587d7fd7d311f9b99bb8439ab4
ndy <ratcomp9992@gmail.com>
2024-06-07 22:01:52 +0900

    chore(#132): 후회 아이템 이미지 URL을 ID로 변경

--- porko-service/src/main/java/io/porko/consumption/domain/RegretItem.java
+++ porko-service/src/main/java/io/porko/consumption/domain/RegretItem.java
@@ -9,55 +9,55 @@ import java.util.Arrays;
 @AllArgsConstructor
 @RequiredArgsConstructor
 public enum RegretItem {
-    ZERO ("ZERO", 0) {
+    ZERO ("ZERO", 0, 1) {
         @Override
         boolean isMatched(BigDecimal cost) {
             return isRange(startRange, cost);
         }
     },
-    COFFEE ("커피 한 잔", 0, 5000) {
+    COFFEE ("커피 한 잔", 0, 5000, 2) {
         @Override
         boolean isMatched(BigDecimal cost) {
             return isRange(startRange, endRange, cost);
         }
     },
-    HAMBURGER("햄버거 세트", 5000, 10000) {
+    HAMBURGER("햄버거 세트", 5000, 10000, 3) {
         @Override
         boolean isMatched(BigDecimal cost) {
             return isRange(startRange, endRange, cost);
         }
     },
-    CHICKEN("치킨 두 마리", 10000, 50000) {
+    CHICKEN("치킨 두 마리", 10000, 50000, 4) {
         @Override
         boolean isMatched(BigDecimal cost) {
             return isRange(startRange, endRange, cost);
         }
     },
-    SUSHI("스시 오마카세", 50000, 100000) {
+    SUSHI("스시 오마카세", 50000, 100000, 5) {
         @Override
         boolean isMatched(BigDecimal cost) {
             return isRange(startRange, endRange, cost);
         }
     },
-    APPLE_PENCLE("애플 펜슬", 100000, 300000) {
+    APPLE_PENCLE("애플 펜슬", 100000, 300000, 6) {
         @Override
         boolean isMatched(BigDecimal cost) {
             return isRange(startRange, endRange, cost);
         }
     },
-    APPLE_WATCH("애플 워치 SE", 300000, 500000) {
+    APPLE_WATCH("애플 워치 SE", 300000, 500000, 7) {
         @Override
         boolean isMatched(BigDecimal cost) {
             return isRange(startRange, endRange, cost);
         }
     },
-    AIRPODS("에어팟 맥스", 500000, 1000000) {
+    AIRPODS("에어팟 맥스", 500000, 1000000, 8) {
         @Override
         boolean isMatched(BigDecimal cost) {
             return isRange(startRange, endRange, cost);
         }
     },
-    MACBOOK("최신형 맥북", 1000000) {
+    MACBOOK("최신형 맥북", 1000000, 9) {
         @Override
         boolean isMatched(BigDecimal cost) {
             return isRange(startRange, cost);
@@ -67,6 +67,7 @@ public enum RegretItem {
     public final String item;
     final int startRange;
     int endRange;
+    final int itemId;
 
     abstract boolean isMatched(BigDecimal cost);
 
@@ -86,10 +87,6 @@ public enum RegretItem {
             return cost.compareTo(BigDecimal.valueOf(start)) == 0;
         }
         return cost.compareTo(BigDecimal.valueOf(start)) >= 0;
-    }
-
-    public static String getImageURL() {
-        return "ImageURL";
     }
 
     public static RegretItem getRegretItemBymonthlyUsedWithRegret(BigDecimal monthlyUsedWithRegret) {
