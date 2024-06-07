c70dd77081739f038fa6a91282226de8c74cf279
ndy <ratcomp9992@gmail.com>
2024-06-08 06:59:33 +0900

    chore(#140): Entity @Getter 추가

--- porko-service/src/main/java/io/porko/friend/domain/Friend.java
+++ porko-service/src/main/java/io/porko/friend/domain/Friend.java
@@ -3,9 +3,11 @@ package io.porko.friend.domain;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Friend {
     @EmbeddedId
