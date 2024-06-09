1584d82d338288026cd82a4f073428007669c122
choi-ys <project.log.062@gmail.com>
2024-06-09 14:09:50 +0900

    chore(#136): 불 필요 코드 정리

--- porko-service/src/main/java/io/porko/history/exception/HistoryErrorCode.java
+++ porko-service/src/main/java/io/porko/history/exception/HistoryErrorCode.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 public enum HistoryErrorCode {
     HISTORY_NOT_FOUND(NOT_FOUND, "요청하신 히스토리를 찾을 수 없습니다. [historyId: %s]"),
     HISTORY_INVALID_DATE_RANGE(BAD_REQUEST, "요청하신 날짜 범위가 유효하지 않습니다. [startDate: %s, endDate: %s]"),
-    NOT_MATCHED_EXPENSE_CATEGORY(INTERNAL_SERVER_ERROR, "카테고리 매칭에 실패하였습니다. [request: %s]");;
+    NOT_MATCHED_EXPENSE_CATEGORY(INTERNAL_SERVER_ERROR, "카테고리 매칭에 실패하였습니다. [request: %s]");
 
     private final HttpStatus status;
     private final String message;
