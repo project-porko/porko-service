b398aa2ad8ab64ee32fe850f557dde34af64b988
choi-ys <project.log.062@gmail.com>
2024-06-09 14:33:37 +0900

    fix(#136): classpath file path 수정

--- porko-service/src/main/java/io/porko/account/csv/CsvToJavaBeansConverter.java
+++ porko-service/src/main/java/io/porko/account/csv/CsvToJavaBeansConverter.java
@@ -1,19 +1,20 @@
 package io.porko.account.csv;
 
 import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
+import org.springframework.core.io.ClassPathResource;
 
 public class CsvToJavaBeansConverter {
     public static List<TransactionRecordCsvBean> getTransactionRecordCsvBeans(String fileName, Class clazz) throws URISyntaxException, IOException {
-        Path path = Paths.get(ClassLoader.getSystemResource(fileName).toURI());
-        Reader reader = Files.newBufferedReader(path);
-        return new CsvToBeanBuilder<CsvBean>(reader)
+        File file = new ClassPathResource(fileName).getFile();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+        return new CsvToBeanBuilder<CsvBean>(bufferedReader)
             .withType(clazz)
             .build()
             .parse();
