f4defdedab0c538e322bc5337ccb155ccc049c92
choi-ys <project.log.062@gmail.com>
2024-06-11 03:00:17 +0900

    feat(#160): 사용자의 입/출금 내역 조회 시 페이징 처리 추가

--- porko-service/src/main/java/io/porko/history/service/HistoryService.java
+++ porko-service/src/main/java/io/porko/history/service/HistoryService.java
@@ -7,15 +7,11 @@ import io.porko.history.controller.model.HistoryDetailResponse;
 import io.porko.history.controller.model.HistoryListResponse;
 import io.porko.history.controller.model.HistoryResponse;
 import io.porko.history.domain.History;
-import io.porko.history.repo.HistoryQueryRepo;
 import io.porko.history.exception.HistoryErrorCode;
 import io.porko.history.exception.HistoryException;
+import io.porko.history.repo.HistoryQueryRepo;
 import io.porko.history.repo.HistoryRepo;
-import io.porko.member.repo.MemberQueryRepo;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
+import io.porko.pagination.response.PageResponse;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
@@ -25,6 +21,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
@@ -32,59 +33,57 @@ import java.util.stream.Collectors;
 public class HistoryService {
     private final HistoryRepo historyRepo;
     private final HistoryQueryRepo historyQueryRepo;
-    private final MemberQueryRepo memberQueryRepo;
     private final BudgetService budgetService;
 
-    public HistoryListResponse getThisMonthHistoryList(Long loginMemberId) {
+    public HistoryListResponse<History, HistoryResponse> getThisMonthHistoryList(Long loginMemberId, Pageable pageable) {
         YearMonth thisMonth = YearMonth.now();
         LocalDate startDate = thisMonth.atDay(1);
         LocalDate endDate = thisMonth.atEndOfMonth();
 
-        return fetchHistoryList(loginMemberId, startDate, endDate);
+        return fetchHistoryList(loginMemberId, startDate, endDate, pageable);
     }
 
-    public HistoryListResponse getHistoryListByDate(Long loginMemberId, LocalDate startDate, LocalDate endDate) {
-        return fetchHistoryList(loginMemberId, startDate, endDate);
+    public HistoryListResponse<History, HistoryResponse> getHistoryListByDate(Long loginMemberId, LocalDate startDate, LocalDate endDate,
+        Pageable pageable) {
+        return fetchHistoryList(loginMemberId, startDate, endDate, pageable);
     }
 
     public HistoryDetailResponse getHistoryDetail(Long historyId) {
         History history = historyRepo.findById(historyId)
-                .orElseThrow(() -> new HistoryException(HistoryErrorCode.HISTORY_INVALID_DATE_RANGE, historyId));
+            .orElseThrow(() -> new HistoryException(HistoryErrorCode.HISTORY_NOT_FOUND, historyId));
         return HistoryDetailResponse.ofDetail(history);
     }
 
     @Transactional
     public HistoryResponse updateRegretStatus(Long historyId, Boolean regret) {
         History history = historyRepo.findById(historyId)
-                .orElseThrow(() -> new HistoryException(HistoryErrorCode.HISTORY_NOT_FOUND ,historyId));
+            .orElseThrow(() -> new HistoryException(HistoryErrorCode.HISTORY_NOT_FOUND, historyId));
         history.updateRegret(regret);
         historyRepo.save(history);
         return HistoryResponse.of(history);
     }
 
-    public HistoryListResponse fetchHistoryList(Long loginMemberId, LocalDate startDate, LocalDate endDate) {
-        loadMemberById(loginMemberId);
-
+    public HistoryListResponse<History, HistoryResponse> fetchHistoryList(Long loginMemberId, LocalDate startDate, LocalDate endDate,
+        Pageable pageable) {
         LocalDateTime startDateTime = startDate.atStartOfDay();
         LocalDateTime endDateTime = endDate.atTime(23, 59, 59);
 
         BigDecimal totalSpent = historyRepo.calcSpentCostForPeriod(loginMemberId, startDateTime, endDateTime).orElse(BigDecimal.ZERO);
         BigDecimal totalEarned = historyRepo.calcEarnedCostForPeriod(loginMemberId, startDateTime, endDateTime).orElse(BigDecimal.ZERO);
 
-        List<History> histories = historyRepo.findByMemberIdAndUsedAtBetween(loginMemberId, startDateTime, endDateTime);
+        Page<History> histories = historyRepo.findByMemberIdAndUsedAtBetween(loginMemberId, startDateTime, endDateTime, pageable);
 
         List<HistoryResponse> historyResponses = histories.stream()
-                .map(HistoryResponse::of)
-                .collect(Collectors.toList());
+            .map(HistoryResponse::of)
+            .collect(Collectors.toList());
 
-        return HistoryListResponse.of(historyResponses, totalSpent, totalEarned);
+        return HistoryListResponse.of(
+            totalSpent,
+            totalEarned,
+            PageResponse.of(histories, historyResponses)
+        );
     }
 
-    private void loadMemberById(Long id) {
-        memberQueryRepo.findMemberById(id)
-                .orElseThrow(() -> new RuntimeException("Member not found with id: " + id));
-    }
- 
     public List<CalendarResponse> getCalendar(int year, int month, Long memberId) {
         YearMonth yearMonth = YearMonth.of(year, month);
         List<CalendarResponse> calendarResponseList = new ArrayList<>();
@@ -93,21 +92,21 @@ public class HistoryService {
         LocalDate lastDayOfMonth = getLastDayOfMonth(yearMonth);
 
         for (LocalDate date = firstDayOfMonth;
-             date.isBefore(lastDayOfMonth) || date.isEqual(lastDayOfMonth);
-             date = date.plusDays(1)) {
+            date.isBefore(lastDayOfMonth) || date.isEqual(lastDayOfMonth);
+            date = date.plusDays(1)) {
 
             BigDecimal dailyUsedCost = calcDailyUsedCost(date, memberId);
             BigDecimal dailyEarnedCost = calcDailyEarnedCost(date, memberId);
 
             calendarResponseList.add(
-                    CalendarResponse.of(
-                            date,
-                            dailyUsedCost,
-                            dailyEarnedCost,
-                            Optional.ofNullable(calcDailyUsedRate(date, memberId, dailyUsedCost))
-                                    .map(dailyUsedRate -> Weather.getWeatherByDailyUsed(dailyUsedRate).weatherImageNo)
-                                    .orElse(null)
-                    )
+                CalendarResponse.of(
+                    date,
+                    dailyUsedCost,
+                    dailyEarnedCost,
+                    Optional.ofNullable(calcDailyUsedRate(date, memberId, dailyUsedCost))
+                        .map(dailyUsedRate -> Weather.getWeatherByDailyUsed(dailyUsedRate).weatherImageNo)
+                        .orElse(null)
+                )
             );
         }
 
@@ -116,7 +115,7 @@ public class HistoryService {
 
     public LocalDate getLastDayOfMonth(YearMonth yearMonth) {
         if (yearMonth.getYear() == LocalDate.now().getYear()
-                && yearMonth.getMonthValue() == LocalDate.now().getMonthValue()) {
+            && yearMonth.getMonthValue() == LocalDate.now().getMonthValue()) {
             return LocalDate.now();
         } else {
             return yearMonth.atEndOfMonth();
@@ -125,28 +124,28 @@ public class HistoryService {
 
     private BigDecimal calcDailyUsedCost(LocalDate date, Long memberId) {
         return historyQueryRepo.calcDailyUsedCost(date, memberId)
-                .orElse(BigDecimal.ZERO)
-                .stripTrailingZeros();
+            .orElse(BigDecimal.ZERO)
+            .stripTrailingZeros();
     }
 
     private BigDecimal calcDailyEarnedCost(LocalDate date, Long memberId) {
         return historyQueryRepo.calcDailyEarnedCost(date, memberId)
-                .orElse(BigDecimal.ZERO)
-                .stripTrailingZeros();
+            .orElse(BigDecimal.ZERO)
+            .stripTrailingZeros();
     }
 
     private BigDecimal calcDailyUsedRate(LocalDate date, Long memberId, BigDecimal dailyUsedCost) {
         return Optional.ofNullable(budgetService.calcDailyBudget(date, memberId))
-                .map(dailyBudget -> {
-                    if (dailyBudget.equals(BigDecimal.ZERO)) {
-                        return dailyBudget;
-                    } else {
-                        return dailyUsedCost.divide(dailyBudget, 2, RoundingMode.HALF_UP)
-                                .abs()
-                                .multiply(BigDecimal.valueOf(100))
-                                .stripTrailingZeros();
-                    }
-                })
-                .orElse(null);
+            .map(dailyBudget -> {
+                if (dailyBudget.equals(BigDecimal.ZERO)) {
+                    return dailyBudget;
+                } else {
+                    return dailyUsedCost.divide(dailyBudget, 2, RoundingMode.HALF_UP)
+                        .abs()
+                        .multiply(BigDecimal.valueOf(100))
+                        .stripTrailingZeros();
+                }
+            })
+            .orElse(null);
     }
 }
