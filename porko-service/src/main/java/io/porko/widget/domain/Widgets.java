669eaef52c1cd08ad9c2b1a30c2432e729a6fbb7
choi-ys <project.log.062@gmail.com>
2024-06-15 00:13:50 +0900

    refactor: 초기 위젯 목록 생성 Event 처리 방법 변경
    - AS-IS : ApplicationReadyEvent를 직접 사용
    - TO-BE : ApplicationReadyEvent에서 초기 위젯 목록 생성 Event를 추가로 발행

--- porko-service/src/main/java/io/porko/widget/domain/Widgets.java
+++ porko-service/src/main/java/io/porko/widget/domain/Widgets.java
@@ -1,11 +1,20 @@
 package io.porko.widget.domain;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record Widgets(
     List<Widget> elements
 ) {
     public static Widgets from(List<Widget> elements) {
+        return new Widgets(elements);
+    }
+
+    public static Widgets initialWidgets() {
+        List<Widget> elements = Arrays.stream(WidgetCode.values())
+            .map(Widget::from)
+            .collect(Collectors.toList());
         return new Widgets(elements);
     }
 }
--- porko-service/src/test/java/io/porko/widget/event/CreateInitialWidgetsEventListenerTest.java
+++ porko-service/src/test/java/io/porko/widget/event/CreateInitialWidgetsEventListenerTest.java
@@ -5,13 +5,15 @@ import static org.mockito.Mockito.verify;
 import io.porko.config.base.EventListenerTestBase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 @DisplayName("Event:CreateWidgetsEventListener")
-class CreateWidgetsEventListenerTest extends EventListenerTestBase {
+class CreateInitialWidgetsEventListenerTest extends EventListenerTestBase {
     @Test
     @DisplayName("Spring Context 초기화 완료 시, 위젯 초기화 로직 이벤트 실행")
-    void onApplicationReadyCreationWidgets() throws Exception {
+    void onApplicationReadyCreationWidgets() {
         // Then
-        verify(createWidgetsEventListener).onApplicationReadyCreationWidgets();
+        ArgumentCaptor<CreateInitialWidgetsEvent> eventCaptor = ArgumentCaptor.forClass(CreateInitialWidgetsEvent.class);
+        verify(createWidgetsEventListener).onApplicationReadyCreationWidgets(eventCaptor.capture());
     }
 }
