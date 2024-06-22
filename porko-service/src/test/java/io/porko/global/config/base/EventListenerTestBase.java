aedd16a44a827daa61eea25360e70ede6cd9378a
choi-ys <project.log.062@gmail.com>
2024-06-22 16:41:41 +0900

    chore: 개선 관련 참고 링크 추가

--- porko-service/src/test/java/io/porko/global/config/base/EventListenerTestBase.java
+++ porko-service/src/test/java/io/porko/global/config/base/EventListenerTestBase.java
@@ -6,6 +6,10 @@ import io.porko.domain.widget.event.CreateInitialMemberWidgetsEventListener;
 import io.porko.domain.widget.event.CreateWidgetsEventListener;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+// TODO https://www.baeldung.com/spring-test-application-events
+// TODO https://www.baeldung.com/spring-test-application-events
+// TODO https://www.baeldung.com/spring-testexecutionlistener
+// TODO https://miiiinju.tistory.com/21
 public class EventListenerTestBase extends EventTestBase {
     @MockBean
     protected InitializeTransactionDataEventListener initializeTransactionDataEventListener;
